6b0013b7fcf22a89499689d691a484b2627303f0
Julien M <julien.moura@oslandia.com>
2021-07-22 22:14:11 +0200

    Bump deps

--- setup.py
+++ setup.py
@@ -18,10 +18,11 @@ setup(
     ),
     include_package_data=True,
     install_requires=[
-        "click>=7.1,<8",
-        "click-keyring>=0.1,<0.3",
+        "click>=7.1,<9",
         "click-log>=0.3,<0.4",
         "click-spinner>=0.1,<0.2",
+        "lxml>=4.6,<4.7",
+        "python-dotenv>=0.17,<0.19",
         "send2trash==1.6.0b1",
     ],
     entry_points="""
