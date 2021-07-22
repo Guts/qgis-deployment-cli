a18d0b18cc287cbc645c20955cc0ce07f57c7b68
Julien M <julien.moura@oslandia.com>
2021-07-22 21:32:40 +0200

    Change settings path

--- tests/test_cli_check.py
+++ tests/test_cli_check.py
@@ -29,7 +29,7 @@ def test_check_help():
     """Test help command"""
     runner = CliRunner()
     result = runner.invoke(
-        qgis_deployment_toolbelt, ["--settings=./.env.example", "check", "--help"]
+        qgis_deployment_toolbelt, ["--settings=.env.example", "check", "--help"]
     )
     assert result.exit_code == 0
 
