cfd5f5f92d9aaed5671ec036dc4c760d321b864a
Julien M <julien.moura@oslandia.com>
2022-05-06 12:32:32 +0200

    Script testing pathlib capacity for copy/move files

---
+++ tests/dev/dev_copy_files_pathlib.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+
+t = Path("requirements.txt")
+
+# déplacer un fichier
+# t.rename("tests/requirements.txt")
+
+# déplace et écrase
+# t.replace("tests/requirements.txt")
