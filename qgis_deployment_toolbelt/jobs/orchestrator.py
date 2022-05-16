3d7e020c04567d54a79d144d4e8835cbe2835d67
Julien <dev@ingeoveritas.com>
2022-05-16 16:53:13 +0200

    clean up

--- qgis_deployment_toolbelt/jobs/orchestrator.py
+++ qgis_deployment_toolbelt/jobs/orchestrator.py
@@ -61,13 +61,23 @@ class JobsOrchestrator:
         return tuple([job.ID for job in self.JOBS])
 
     def get_job_module_from_id(self, job_id: str) -> object:
-        """Get job class from id."""
+        """Get job class from id.
+
+        :param str job_id: job identifier (as defined in scenario file)
+        :return object: Job class
+        """
         for job in self.JOBS:
             if job.ID == job_id:
                 return job
 
     def init_job_class_from_id(self, job_id: str, options: dict) -> object:
-        """Get job class from id and instanciate it with options."""
+        """Get job class from id and instanciate it with options.
+
+        :param str job_id: job identifier (i.e. "qprofiles-manager")
+        :param dict options: job options
+
+        :return object: instanciated job class with options
+        """
         return self.get_job_module_from_id(job_id)(options)
 
     def import_job(self, job_name: str):
@@ -86,62 +96,4 @@ class JobsOrchestrator:
 
 if __name__ == "__main__":
     """Standalone execution."""
-    import inspect
-    from pprint import pprint
-
-    from generic import JobBase
-
-    # mod = importlib.import_module(
-    #     name="job_settings", package="qgis_deployment_toolbelt.jobs"
-    # )
-    # print(mod.__doc__, mod.__name__, dir(mod))
-
-    orchestrator = JobsOrchestrator()
-    jobs_available = orchestrator.available_jobs
-
-    j = orchestrator.import_job("job_settings")
-    print(j.__doc__, j.__name__, dir(j))
-
-    # orchestrator.import_all_jobs()
-
-    # pprint(orchestrator.JOBS)
-
-    # for k, v in orchestrator.JOBS.items():
-    #     print(v, type(v), dir(v))
-    #     for name_local in dir(v):
-    #         if name_local.startswith("__"):
-    #             continue
-    #         module_class = getattr(v, name_local)
-    #         print(str(module_class), type(module_class))
-    # #         print(name_local, isclass(getattr(v, name_local)))
-    #         # print(name_local, issubclass(getattr(v, name_local), JobBase))
-    #         print(module_class, issubclass(getattr(v, module_class), JobBase))
-    # #         print(name_local, isinstance(getattr(v, name_local), JobBase))
-    #         if isclass(getattr(v, name_local)) and isinstance(getattr(v, name_local), JobBase):
-    #             print("test " + name_local)
-    #             # print(f"{k} - {name_local.ID}")
-    #         # for name_local in dir(v):
-    #         #     if inspect.isclass(getattr(v, name_local)):
-    #         #         print(f'{name_local} is a class')
-    #         #         MysteriousClass = getattr(v, name_local)
-    #         #         mysterious_object = MysteriousClass()
-
-    # READ https://julienharbulot.com/python-dynamical-import.html
-
-    # from inspect import isclass
-    # from pkgutil import iter_modules
-    # from pathlib import Path
-    # from importlib import import_module
-
-    # # iterate through the modules in the current package
-    # package_dir = Path(__file__).resolve().parent
-    # for (_, module_name, _) in iter_modules([package_dir]):
-
-    #     # import the module and iterate through its attributes
-    #     module = import_module(f"{__name__}.{module_name}")
-    #     for attribute_name in dir(module):
-    #         attribute = getattr(module, attribute_name)
-
-    #         if issubclass(attribute, JobBase):
-    #             # Add the class to this package's variables
-    #             globals()[attribute_name] = attribute
+    pass
