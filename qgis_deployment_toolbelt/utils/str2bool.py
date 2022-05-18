3fd11e2ee2cd9907b0dea0d0cbb4da33ed85aa79
Julien M <julien.moura@oslandia.com>
2022-05-18 14:06:40 +0200

    Minor improvmen

--- qgis_deployment_toolbelt/utils/str2bool.py
+++ qgis_deployment_toolbelt/utils/str2bool.py
@@ -16,6 +16,15 @@ _false_set = {"f", "false", "n", "no", "off", "0"}
 # ########## Functions #############
 # ##################################
 def str2bool(value: str, raise_exc: bool = False) -> bool:
+    """Determine if a string is a bool and, if so, convert it.
+
+    :param str value: value to convert
+    :param bool raise_exc: strict mode, defaults to False
+    :raises ValueError: value is not in true and false sets
+
+    :return bool: True if input string is in _true_set, False if it's in _false_set.
+    None or Exception if not in any of two sets.
+    """
     if isinstance(value, str):
         value = value.lower()
         if value in _true_set:
@@ -23,6 +32,9 @@ def str2bool(value: str, raise_exc: bool = False) -> bool:
         if value in _false_set:
             return False
 
-    if raise_exc:
-        raise ValueError('Expected "%s"' % '", "'.join(_true_set | _false_set))
-    return None
+        if raise_exc:
+            raise ValueError('Expected "%s"' % '", "'.join(_true_set | _false_set))
+
+        return None
+    else:
+        raise TypeError(f"value must be a str, not {type(value)}")
