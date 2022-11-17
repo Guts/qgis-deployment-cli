d2e163f6ad3805181cf7a1b19c21c0693ebf9b53
Vincent Bré <vincent.bre@oslandia.com>
2022-11-17 14:54:11 +0100

    update test_main_run_with_disable_validation_option unit test

--- tests/test_cli_main.py
+++ tests/test_cli_main.py
@@ -26,6 +26,9 @@ from qgis_deployment_toolbelt.cli import qgis_deployment_toolbelt
 sample_scenario_good: Path = Path(
     "tests/fixtures/scenarios/good_scenario_sample.qdt.yml"
 )
+sample_scenario_false: Path = Path(
+    "tests/fixtures/scenarios/false_scenario_sample.qdt.yml"
+)
 sample_scenario_imaginary: Path = Path(
     "tests/fixtures/scenarios/imaginary_scenario_sample.qdt.yml"
 )
@@ -61,14 +64,20 @@ def test_main_run():
     assert result.exit_code == 1
 
 
-def test_main_run_with_check_validity_option():
-    """Test main cli command with the check-validity option"""
+def test_main_run_with_disable_validation_option():
+    """Test main cli command with the disable validation option"""
     runner = CliRunner()
     result = runner.invoke(
         qgis_deployment_toolbelt,
-        [f"--scenario={str(sample_scenario_good.resolve())}", "check_validity"],
+        [f"--scenario={str(sample_scenario_good.resolve())}", "--disable-validation"],
     )
-    assert result.exit_code == 2
+    assert result.exit_code == 0
+
+    result = runner.invoke(
+        qgis_deployment_toolbelt,
+        [f"--scenario={str(sample_scenario_false.resolve())}", "--disable-validation"],
+    )
+    assert result.exit_code == 1
 
 
 # #############################################################################
