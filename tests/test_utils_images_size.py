eb7a46ab1f6d8cf2d3aaca344676f32a16aae20e
Julien M <julien.moura@oslandia.com>
2023-01-13 13:04:15 +0100

    Improve tests

--- tests/test_utils_images_size.py
+++ tests/test_utils_images_size.py
@@ -7,17 +7,25 @@
         # for whole tests
         python -m unittest tests.test_utils_images_size
         # for specific test
-        python -m unittest tests.test_utils.TestUtilsImagesSizeChecker.get_svg_size
+        python -m unittest tests.test_utils_images_size.TestUtilsImagesSizeChecker.get_svg_size
 """
 
 
-import unittest
-
 # standard library
+import unittest
 from pathlib import Path
+from tempfile import TemporaryDirectory
+
+# 3rd party
+from PIL import Image
 
 # project
-from qgis_deployment_toolbelt.utils.check_image_size import get_svg_size
+from qgis_deployment_toolbelt.__about__ import __title_clean__, __version__
+from qgis_deployment_toolbelt.utils.check_image_size import (
+    check_image_dimensions,
+    get_image_size,
+    get_svg_size,
+)
 
 # ############################################################################
 # ########## Classes #############
@@ -27,6 +35,28 @@ from qgis_deployment_toolbelt.utils.check_image_size import get_svg_size
 class TestUtilsImagesSizeChecker(unittest.TestCase):
     """Test package utilities."""
 
+    # -- Standard methods --------------------------------------------------------
+    @classmethod
+    def setUpClass(cls) -> None:
+        """Set up test."""
+
+        cls.img_tmp_folder = TemporaryDirectory(
+            prefix=f"{__title_clean__}_{__version__}_"
+        )
+
+        # create a temporary image
+        cls.img_black_800x600 = Path(cls.img_tmp_folder.name, "img_black_800x600.jpg")
+        width = 800
+        height = 600
+        image = Image.new("RGB", (width, height), "black")
+        image.save(cls.img_black_800x600)
+
+    @classmethod
+    def tearDownClass(cls):
+        """Executed after each test."""
+        # cls.img_tmp_folder.cleanup()
+
+    # -- TESTS ---------------------------------------------------------
     def test_svg_size_with_dimensions(self):
         """Test svg size retriever."""
         svg_with_dimensions_attributes = Path(
@@ -35,7 +65,7 @@ class TestUtilsImagesSizeChecker(unittest.TestCase):
 
         self.assertTrue(svg_with_dimensions_attributes.is_file())
         # svg with dimensions set
-        svg_size = get_svg_size(image_filepath=svg_with_dimensions_attributes)
+        svg_size = get_image_size(image_filepath=svg_with_dimensions_attributes)
 
         self.assertIsInstance(svg_size, tuple)
         self.assertIsInstance(svg_size[0], int)
@@ -48,12 +78,16 @@ class TestUtilsImagesSizeChecker(unittest.TestCase):
         svg_without_dimensions_attributes = Path(
             "tests/fixtures/miscellaneous/sample_without_dimensions_attributes.svg"
         )
-
         self.assertTrue(svg_without_dimensions_attributes.is_file())
-        # svg with dimensions set
-        svg_size = get_svg_size(image_filepath=svg_without_dimensions_attributes)
+        svg_size = get_image_size(image_filepath=svg_without_dimensions_attributes)
 
         self.assertIsNone(svg_size)
+
+    def test_get_image_dimensions(self):
+        img_800x600 = get_image_size(self.img_black_800x600)
+        self.assertIsInstance(img_800x600, tuple)
+        self.assertEqual(img_800x600[0], 800)
+        self.assertEqual(img_800x600[1], 600)
 
 
 # ############################################################################
