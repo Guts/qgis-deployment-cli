2e787dc1512d553c0fd01fe750d1f7cdbd1de973
Julien M <julien.moura@oslandia.com>
2023-01-16 15:01:08 +0100

    Rm Python 3.8 and 3.9 support

--- setup.py
+++ setup.py
@@ -49,8 +49,6 @@ setup(
         "Operating System :: Microsoft :: Windows :: Windows 10",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Operating System :: OS Independent",
