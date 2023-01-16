029994d6fc1f64d0e88195bbd63cdc58c1db58bc
Julien M <julien.moura@oslandia.com>
2023-01-16 13:43:29 +0100

    Add minimal test

---
+++ tests/test_cli_upgrade.py
@@ -0,0 +1,44 @@
+#! python3  # noqa: E265
+
+"""
+    Test CLI's clean command.
+
+    Author: Julien Moura (Oslandia)
+"""
+
+# #############################################################################
+# ########## Libraries #############
+# ##################################
+
+import shutil
+
+# Standard library
+import unittest
+from pathlib import Path
+
+# 3rd party library
+from click.testing import CliRunner
+
+# module to test
+from qgis_deployment_toolbelt.cli import qgis_deployment_toolbelt
+
+# #############################################################################
+# ######## Classes #################
+# ##################################
+
+
+def test_upgrade_check():
+    """Test help command"""
+    runner = CliRunner()
+    result = runner.invoke(
+        qgis_deployment_toolbelt,
+        ["upgrade", "-c"],
+    )
+    assert result.exit_code == 0
+
+
+# #############################################################################
+# ######## Standalone ##############
+# ##################################
+if __name__ == "__main__":
+    pass
