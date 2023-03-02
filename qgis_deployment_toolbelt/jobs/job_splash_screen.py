aab68b4e52f13b9494be0295d0a822c3becb7128
Julien M <julien.moura@oslandia.com>
2023-03-02 21:05:43 +0100

    Rm former validations method

--- qgis_deployment_toolbelt/jobs/job_splash_screen.py
+++ qgis_deployment_toolbelt/jobs/job_splash_screen.py
@@ -433,48 +433,6 @@ class JobSplashScreenManager(GenericJob):
             )
             return False
 
-    def validate_options(self, options: dict) -> bool:
-        """Validate options.
-
-        :param dict options: options to validate.
-        :return bool: True if options are valid.
-        """
-        for option in options:
-            if option not in self.OPTIONS_SCHEMA:
-                raise Exception(
-                    f"Job: {self.ID}. Option '{option}' is not valid."
-                    f" Valid options are: {self.OPTIONS_SCHEMA.keys()}"
-                )
-
-            option_in = options.get(option)
-            option_def: dict = self.OPTIONS_SCHEMA.get(option)
-            # check value type
-            if not isinstance(option_in, option_def.get("type")):
-                raise Exception(
-                    f"Job: {self.ID}. Option '{option}' has an invalid value."
-                    f"\nExpected {option_def.get('type')}, got {type(option_in)}"
-                )
-            # check value condition
-            if option_def.get("condition") == "startswith" and not option_in.startswith(
-                option_def.get("possible_values")
-            ):
-                raise Exception(
-                    f"Job: {self.ID}. Option '{option}' has an invalid value."
-                    "\nExpected: starts with one of: "
-                    f"{', '.join(option_def.get('possible_values'))}"
-                )
-            elif option_def.get(
-                "condition"
-            ) == "in" and option_in not in option_def.get("possible_values"):
-                raise Exception(
-                    f"Job: {self.ID}. Option '{option}' has an invalid value."
-                    f"\nExpected: one of: {', '.join(option_def.get('possible_values'))}"
-                )
-            else:
-                pass
-
-        return options
-
 
 # #############################################################################
 # ##### Stand alone program ########
