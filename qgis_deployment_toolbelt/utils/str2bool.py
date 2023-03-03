bd05d0f41c1e5830d872b7df384cb066214b97fd
Julien M <julien.moura@oslandia.com>
2023-03-03 12:40:34 +0100

    Improve str2bool helper

--- qgis_deployment_toolbelt/utils/str2bool.py
+++ qgis_deployment_toolbelt/utils/str2bool.py
@@ -5,36 +5,61 @@
 """
 
 # #############################################################################
+# ########## Libraries #############
+# ##################################
+
+# Standard library
+import logging
+from typing import Optional
+
+# #############################################################################
 # ########## Globals ###############
 # ##################################
 
 _true_set = {"on", "t", "true", "y", "yes", "1"}
 _false_set = {"f", "false", "n", "no", "off", "0"}
-
+logger = logging.getLogger(__name__)
 
 # #############################################################################
 # ########## Functions #############
 # ##################################
-def str2bool(value: str, raise_exc: bool = False) -> bool:
+
+
+def str2bool(input_var: str, raise_exc: bool = False) -> Optional[bool]:
     """Determine if a string is a bool and, if so, convert it.
 
-    :param str value: value to convert
-    :param bool raise_exc: strict mode, defaults to False
-    :raises ValueError: value is not in true and false sets
+    Args:
+        input_var (str): value to convert
+        raise_exc (bool, optional): strict mode, defaults to False. Defaults to False.
 
-    :return bool: True if input string is in _true_set, False if it's in _false_set.
+    Raises:
+        ValueError: input_var is not in true and false sets
+        TypeError: if input_var is not a str or a bool
+
+    Returns:
+        bool: True if input_var is in _true_set, False if it's in _false_set.
     None or Exception if not in any of two sets.
     """
-    if isinstance(value, str):
-        value = value.lower()
+    if isinstance(input_var, str):
+        value = input_var.lower()
         if value in _true_set:
             return True
         if value in _false_set:
             return False
 
+        error_message = 'Expected "%s"' % '", "'.join(_true_set | _false_set)
         if raise_exc:
-            raise ValueError('Expected "%s"' % '", "'.join(_true_set | _false_set))
-
-        return None
+            raise ValueError(error_message)
+        else:
+            logger.error(error_message)
+            return None
+    elif isinstance(input_var, bool):
+        logger.debug(f"Value {input_var} was already a bool.")
+        return input_var
     else:
-        raise TypeError(f"value must be a str, not {type(value)}")
+        error_message = f"Value must be a str, not {type(input_var)}"
+        if raise_exc:
+            raise TypeError(error_message)
+        else:
+            logger.error(error_message)
+            return None
