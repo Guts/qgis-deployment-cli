83195b7ba8d80b09650fafb726daaaa27ebe9f2f
Julien M <julien.moura@oslandia.com>
2023-04-14 14:38:52 +0200

    Bump version to 0.23.0

--- qgis_deployment_toolbelt/__about__.py
+++ qgis_deployment_toolbelt/__about__.py
@@ -36,7 +36,7 @@ __uri_repository__ = "https://github.com/Guts/qgis-deployment-cli/"
 __uri_tracker__ = f"{__uri_repository__}issues/"
 __uri__ = __uri_repository__
 
-__version__ = "0.22.3"
+__version__ = "0.23.0"
 __version_info__ = tuple(
     [
         int(num) if num.isdigit() else num
