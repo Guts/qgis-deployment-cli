0ae0e93fc9fe157bf235f88ee0f72aa602a0c787
Julien M <julien.moura@oslandia.com>
2023-06-12 16:00:58 +0200

    Fix path

--- builder/pyinstaller_build_windows.py
+++ builder/pyinstaller_build_windows.py
@@ -31,7 +31,7 @@ PyInstaller.__main__.run(
         "--add-data=LICENSE;.",
         "--add-data=README.md;.",
         "--clean",
-        f"--icon={package_folder.parent}/docs/static/logo_qdt.png",
+        f"--icon={package_folder.parent.resolve()}/docs/static/logo_qdt.png",
         "--log-level={}".format(getenv("PYINSTALLER_LOG_LEVEL", "WARN")),
         "--manifest={}".format((package_folder / "../builder/manifest.xml").resolve()),
         "--name={}_{}_{}{}_Python{}-{}".format(
