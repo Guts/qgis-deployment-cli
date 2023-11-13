ab29d91ea992dcb5ec88270f32d74bdbd8026d0d
Julien M. <julien.moura@oslandia.com>
2023-11-13 22:20:16 +0100

    Add tests againts splash path test

--- tests/test_qgis_ini_helper.py
+++ tests/test_qgis_ini_helper.py
@@ -5,7 +5,7 @@
 
     .. code-block:: bash
         # for whole tests
-        python -m unittest tests.test_qdt_profile_object
+        python -m unittest tests.test_qgis_ini_helper
         # for specific test
         python -m unittest tests.test_qgis_ini_helper.TestQgisIniHelper.test_profile_load_from_json_basic
 """
@@ -39,7 +39,7 @@ class TestQgisIniHelper(unittest.TestCase):
         )
         ini_config = QgisIniHelper(ini_filepath=new_config_file)
 
-        self.assertEqual(ini_config.INI_TYPE, "profile_qgis3")
+        self.assertEqual(ini_config.ini_type, "profile_qgis3")
         self.assertFalse(ini_config.is_ui_customization_enabled())
 
     def test_load_profile_customization_splash_screen(self):
@@ -49,11 +49,11 @@ class TestQgisIniHelper(unittest.TestCase):
         )
         ini_customization = QgisIniHelper(ini_filepath=new_config_file)
 
-        self.assertEqual(ini_customization.INI_TYPE, "profile_qgis3customization")
+        self.assertEqual(ini_customization.ini_type, "profile_qgis3customization")
 
         ini_config = QgisIniHelper(ini_filepath=ini_customization.profile_config_path)
 
-        self.assertEqual(ini_config.INI_TYPE, "profile_qgis3")
+        self.assertEqual(ini_config.ini_type, "profile_qgis3")
 
     def test_enable_customization(self):
         """Test profile QGIS/QGIS3.ini loader."""
@@ -68,6 +68,56 @@ class TestQgisIniHelper(unittest.TestCase):
 
         ini_config.set_ui_customization_enabled(switch=False)
         self.assertFalse(ini_config.is_ui_customization_enabled())
+
+    def test_splash_already_set(self):
+        fake_config = (
+            "[Customization]\nsplashpath=/home/$USER/.share/QGIS/QGIS3/images/"
+        )
+
+        tmp_ini_customization = Path(
+            "tests/fixtures/tmp/customization_with_splashpath.ini"
+        )
+        tmp_ini_customization.parent.mkdir(parents=True, exist_ok=True)
+        tmp_ini_customization.write_text(fake_config)
+
+        qini_helper = QgisIniHelper(
+            ini_filepath=tmp_ini_customization, ini_type="profile_qgis3customization"
+        )
+        self.assertTrue(qini_helper.is_splash_screen_set())
+
+        tmp_ini_customization.unlink()
+
+    def test_splash_already_set_but_empty(self):
+        fake_config = "[Customization]\nsplashpath="
+
+        tmp_ini_customization = Path(
+            "tests/fixtures/tmp/customization_with_splashpath_empty.ini"
+        )
+        tmp_ini_customization.parent.mkdir(parents=True, exist_ok=True)
+        tmp_ini_customization.write_text(fake_config)
+
+        qini_helper = QgisIniHelper(
+            ini_filepath=tmp_ini_customization, ini_type="profile_qgis3customization"
+        )
+        self.assertFalse(qini_helper.is_splash_screen_set())
+
+        tmp_ini_customization.unlink()
+
+    def test_splash_not_set(self):
+        fake_config = "[Customization]\nMenu\\mDatabaseMenu=false"
+
+        tmp_ini_customization = Path(
+            "tests/fixtures/tmp/customization_with_no_splashpath_set.ini"
+        )
+        tmp_ini_customization.parent.mkdir(parents=True, exist_ok=True)
+        tmp_ini_customization.write_text(fake_config)
+
+        qini_helper = QgisIniHelper(
+            ini_filepath=tmp_ini_customization, ini_type="profile_qgis3customization"
+        )
+        self.assertFalse(qini_helper.is_splash_screen_set())
+
+        tmp_ini_customization.unlink()
 
 
 # ############################################################################
