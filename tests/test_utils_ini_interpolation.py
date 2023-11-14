f8cde982eba6642dc1fc62638f37c5f75857ddcd
Julien M. <julien.moura@oslandia.com>
2023-11-14 10:47:35 +0100

    Split interpolation tests by operating system

--- tests/test_utils_ini_interpolation.py
+++ tests/test_utils_ini_interpolation.py
@@ -30,7 +30,8 @@ from qgis_deployment_toolbelt.utils.ini_interpolation import (
 class TestUtilsIniCustomInterpolation(unittest.TestCase):
     """Test custom INI values' interpolation."""
 
-    def test_ini_env_var_interpolation(self):
+    @unittest.skipIf(opersys == "win32", "Test specific to Linux.")
+    def test_ini_env_var_interpolation_linux(self):
         """Test interpolation results."""
         fixtures_configuration = """
         [test]
@@ -41,8 +42,7 @@ class TestUtilsIniCustomInterpolation(unittest.TestCase):
 
         """
         environ["QDT_TEST_ENV_VARIABLE"] = "TEST_VALUE"
-        if opersys == "win32":
-            environ["USER"] = getuser()
+
         config = ConfigParser(interpolation=EnvironmentVariablesInterpolation())
         config.read_string(fixtures_configuration)
         self.assertEqual(config.get(section="test", option="user"), getuser())
@@ -55,6 +55,36 @@ class TestUtilsIniCustomInterpolation(unittest.TestCase):
             config.get(section="test", option="qdt_working_directory"),
             f"{config.get(section='test', option='user_home')}/.cache/qgis-deployment-toolbelt",
         )
+
+        environ.pop("QDT_TEST_ENV_VARIABLE")
+
+    @unittest.skipIf(opersys != "win32", "Test specific to Windows.")
+    def test_ini_env_var_interpolation_windows(self):
+        """Test interpolation results."""
+        fixtures_configuration = """
+        [test]
+        user = $USERNAME
+        user_home = $USERPROFILE
+        fake_value_from_environment_variable = $QDT_TEST_ENV_VARIABLE
+        qdt_working_directory = %(user_home)s/.cache/qgis-deployment-toolbelt
+
+        """
+        environ["QDT_TEST_ENV_VARIABLE"] = "TEST_VALUE"
+
+        config = ConfigParser(interpolation=EnvironmentVariablesInterpolation())
+        config.read_string(fixtures_configuration)
+        self.assertEqual(config.get(section="test", option="user"), getuser())
+        self.assertEqual(config.get(section="test", option="user_home"), getenv("HOME"))
+        self.assertEqual(
+            config.get(section="test", option="fake_value_from_environment_variable"),
+            getenv("QDT_TEST_ENV_VARIABLE"),
+        )
+        self.assertEqual(
+            config.get(section="test", option="qdt_working_directory"),
+            f"{config.get(section='test', option='user_home')}/.cache/qgis-deployment-toolbelt",
+        )
+
+        environ.pop("QDT_TEST_ENV_VARIABLE")
 
 
 # ############################################################################
