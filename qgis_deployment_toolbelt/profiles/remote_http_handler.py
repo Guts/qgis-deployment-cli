ff110d3fff619817c12564fbea6febe3e62482e9
Julien M <julien.moura@oslandia.com>
2023-12-28 19:52:10 +0100

    feature: improve parsing and downloading remote files

--- qgis_deployment_toolbelt/profiles/remote_http_handler.py
+++ qgis_deployment_toolbelt/profiles/remote_http_handler.py
@@ -14,15 +14,20 @@
 
 # Standard library
 import logging
+from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
+from typing import TypedDict
 
 # 3rd party
 import requests
 
 # project
+from qgis_deployment_toolbelt.__about__ import __title_clean__, __version__
 from qgis_deployment_toolbelt.profiles.profiles_handler_base import (
     RemoteProfilesHandlerBase,
 )
+from qgis_deployment_toolbelt.utils.file_downloader import download_remote_file_to_local
+from qgis_deployment_toolbelt.utils.formatters import url_ensure_trailing_slash
 from qgis_deployment_toolbelt.utils.proxies import get_proxy_settings
 
 # #############################################################################
@@ -31,6 +36,12 @@ from qgis_deployment_toolbelt.utils.proxies import get_proxy_settings
 
 # logs
 logger = logging.getLogger(__name__)
+
+
+class Treeitem(TypedDict):
+    type: str
+    name: str
+    contents: list[dict] | None
 
 
 # #############################################################################
@@ -45,6 +56,11 @@ class HttpHandler(RemoteProfilesHandlerBase):
     - the local repository (destination) is on a local network or drive
     """
 
+    # headers
+    HTTP_HEADERS = {
+        "User-Agent": f"{__title_clean__}/{__version__}",
+    }
+
     def __init__(
         self,
         source_repository_path_or_uri: str,
@@ -55,16 +71,10 @@ class HttpHandler(RemoteProfilesHandlerBase):
         Args:
             source_repository_path_or_uri (str | Path): path to the source repository
         """
-        self.SOURCE_REPOSITORY_PATH_OR_URL = source_repository_path_or_uri
+        self.SOURCE_REPOSITORY_PATH_OR_URL = url_ensure_trailing_slash(
+            source_repository_path_or_uri
+        )
         super().__init__(source_repository_type=source_repository_type)
-
-        self.SOURCE_REPOSITORY_PATH_OR_URL = source_repository_path_or_uri
-
-        # make sure that URL has a trailing slash
-        if isinstance(
-            self.SOURCE_REPOSITORY_PATH_OR_URL, str
-        ) and not self.SOURCE_REPOSITORY_PATH_OR_URL.endswith("/"):
-            self.SOURCE_REPOSITORY_PATH_OR_URL += "/"
 
     def download(self, destination_local_path: Path):
         """Generic wrapper around the specific logic of this handler.
@@ -78,13 +88,22 @@ class HttpHandler(RemoteProfilesHandlerBase):
             f"{destination_local_path}"
         )
 
-        # get qdt-files.json
-        req = requests.get(
-            url=f"{self.SOURCE_REPOSITORY_PATH_OR_URL}/qdt-files.json",
-            proxies=get_proxy_settings(),
-        )
-        req.raise_for_status()
-        data = req.json()
+        try:
+            logger.debug("Retrieve qdt-files.json ")
+            # get qdt-files.json
+            req = requests.get(
+                url=f"{self.SOURCE_REPOSITORY_PATH_OR_URL}qdt-files.json",
+                headers=self.HTTP_HEADERS,
+                proxies=get_proxy_settings(),
+            )
+            req.raise_for_status()
+            qdt_tree = req.json()
+        except Exception as err:
+            logger.critical(
+                f"Downloading {self.SOURCE_REPOSITORY_PATH_OR_URL} to "
+                f"{destination_local_path} failed. Trace: {err}"
+            )
+            raise err
 
         # make sure destination path exists
         if not destination_local_path.exists():
@@ -94,42 +113,96 @@ class HttpHandler(RemoteProfilesHandlerBase):
             )
             destination_local_path.mkdir(parents=True)
 
-        self.recreate_local_structure(contents=data, parent_path=destination_local_path)
+        li_files_to_download = self.tree_to_download_list(tree_array=qdt_tree)
+        logger.info(f"{len(li_files_to_download)} files to download")
 
-    def recreate_local_structure(self, contents: list, parent_path: Path):
-        """
-        Crée une structure de dossiers et télécharge des fichiers basée sur la structure JSON.
+        success, fails = self.download_files_to_local(
+            li_files_to_download=li_files_to_download,
+            target_folder=destination_local_path,
+        )
+        if not len(success):
+            logger.error("No files downloaded! Please check the above log messages.")
+        if len(fails):
+            logger.warning(
+                f"{len(fails)} download failed. Check the above log messages."
+            )
 
-        :param contents: Liste de dictionnaires représentant les fichiers et dossiers.
-        :param parent_path: Chemin du dossier parent dans lequel la structure sera créée.
+    def tree_to_download_list(
+        self, tree_array: list[Treeitem], rel_path: str = ""
+    ) -> list:
+        """Parse tree structure and return a list of files to download with relative
+            paths to the base URL. It's meant to be used as recursive funciton to iter
+            through the tree structure.
+
+        Args:
+            tree_array (list[TreeItem]): input array from tree JSON structure.
+            rel_path (str, optional): relative path to resolve from. Defaults to "".
+
+        Returns:
+            list: list of files paths relative to the base URL.
         """
-        for item in contents:
-            if item["type"] == "directory":
-                logger.debug(
-                    f"Folder detected: {item}. Creating local folder: {parent_path.joinpath(item.get('name'))}"
-                )
-                if item.get("name") == ".":
-                    dir_path = parent_path
+        li_files = []
+
+        for item in tree_array:
+            if item.get("type") == "directory":
+                if item.get("name") != ".":
+                    new_rel_path = f"{rel_path}/{item.get('name')}"
                 else:
-                    dir_path = parent_path.joinpath(item.get("name"))
-                    dir_path.mkdir(parents=True, exist_ok=True)
+                    new_rel_path = f"{item.get('name')}"
 
-                self.recreate_local_structure(
-                    contents=item.get("contents"), parent_path=dir_path
+                li_files.extend(
+                    self.tree_to_download_list(
+                        tree_array=item.get("contents"),
+                        rel_path=new_rel_path,
+                    )
                 )
-            elif item["type"] == "file":
-                file_path = parent_path.joinpath(item.get("name"))
-                logger.debug(
-                    f"File detected. Downloading remote file {item.get('name')} to  local folder: {parent_path.joinpath(item.get('name'))}"
-                )
-                file_url = f"{self.SOURCE_REPOSITORY_PATH_OR_URL}{file_path}"
+            elif item.get("type") == "file":
+                li_files.append(f"{rel_path}/{item.get('name')}")
+            else:
+                logger.debug(f"Unsupported item type: {item.get('type')}")
 
-                with requests.get(
-                    url=file_url, stream=True, proxies=get_proxy_settings()
-                ) as response:
-                    with file_path.open(mode="wb") as file:
-                        for chunk in response.iter_content(chunk_size=10 * 1024):
-                            file.write(chunk)
+        return li_files
+
+    def download_files_to_local(
+        self, li_files_to_download: list[str], target_folder: Path
+    ) -> tuple[list[tuple[str, Path]], list[tuple[str, Path]]]:
+        """Download list of files relative to remote base URL to local target folder.
+
+        Args:
+            li_files_to_download (list[str]): list of files to download.
+            target_folder (Path): local folder where to download
+
+        Returns:
+            tuple[list[tuple[str, Path]], list[tuple[str, Path]]]: (list of success \
+                download, list of failed download)
+        """
+        base_url = self.SOURCE_REPOSITORY_PATH_OR_URL
+        downloaded_files: list[tuple[str, Path]] = []
+        failed_files: list[tuple[str, str]] = []
+
+        with ThreadPoolExecutor(
+            thread_name_prefix=f"{__title_clean__}_profile_dl_http_"
+        ) as executor:
+            for file_to_download in li_files_to_download:
+                # submit download to pool
+                try:
+                    executor.submit(
+                        # func to execute
+                        download_remote_file_to_local,
+                        # func parameters
+                        local_file_path=target_folder.joinpath(file_to_download),
+                        remote_url_to_download=f"{base_url}{file_to_download}",
+                    )
+                    downloaded_files.append(
+                        (
+                            f"{base_url}{file_to_download}",
+                            target_folder.joinpath(file_to_download),
+                        )
+                    )
+                except Exception as err:
+                    failed_files.append((file_to_download, f"{err}"))
+
+        return downloaded_files, failed_files
 
 
 # #############################################################################
