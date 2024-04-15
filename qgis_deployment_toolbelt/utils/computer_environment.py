4ee43527dacce69d20d0b20239922420605144bb
Julien <julien.moura@oslandia.com>
2024-04-15 17:56:05 +0200

    fix: make sure that environment dict is cross-platform

--- qgis_deployment_toolbelt/utils/computer_environment.py
+++ qgis_deployment_toolbelt/utils/computer_environment.py
@@ -34,15 +34,25 @@ def environment_dict() -> dict:
         platform) that can be used in QDT various places: rules...
 
     Returns:
-        dict: _description_
+        dict: dict with some environment metadata to use in rules.
     """
+    try:
+        linux_distribution_name = f"{platform.freedesktop_os_release().get('NAME')}"
+        linux_distribution_version = (
+            f"{platform.freedesktop_os_release().get('VERSION_ID')}"
+        )
+    except OSError as err:
+        logger.debug(f"Trace: {err}.")
+        linux_distribution_name = None
+        linux_distribution_version = None
+
     return {
         "computer_network_name": platform.node(),
         "operating_system_code": opersys,
         "processor_architecture": platform.machine(),
         # custom Linux
-        "linux_distribution_name": f"{platform.freedesktop_os_release().get('NAME')}",
-        "linux_distribution_version": f"{platform.freedesktop_os_release().get('VERSION_ID')}",
+        "linux_distribution_name": linux_distribution_name,
+        "linux_distribution_version": linux_distribution_version,
         # custom Windows
         "windows_edition": platform.win32_edition(),
     }
